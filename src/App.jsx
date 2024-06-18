e3e03603f21b9d771bb409af6410d0082123e828
Raquel Benito <raquelbenitop@gmail.com>
2024-06-18 18:59:47 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import './App.css'
 
+import {useState} from "react"
+import ReactDOM from 'react-dom/client'
+
+
 function App() {
- //Aquí tu código
+ const [name, setName] = useState("Sofía")
+ const [newName, setNewName] = useState("")
+
+ function changeName (newName){
+    setName(newName)
+ }
+
+ return(
+    <div>
+      <h2>Teacher Name: {name}</h2>
+      <ul>
+        <li> <button onClick={() => changeName('Data')}>Data</button> </li>
+        <li> <button onClick={() => changeName('Reyes')}>Reyes</button> </li>
+        <li> <button onClick={() => changeName('Yolanda')}>Yolanda</button> </li>
+      </ul>
+    </div>
+)
 }
 
+
+
 export default App
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>)
